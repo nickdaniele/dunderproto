6eaa1e435b420e41cf6f28ea0a117a9e2d8635f3
Nick Daniele <ngdaniele@gmail.com>
2018-10-20 13:39:59 -0700

    Added plugins for caching.

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,9 +1,13 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
+const webpack = require('webpack');
 
 module.exports = merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
+  plugins: [
+    new webpack.NamedModulesPlugin(),
+  ],
   devServer: {
     contentBase: './dist',
     hot: true,
--- webpack.prod.js
+++ webpack.prod.js
@@ -2,10 +2,14 @@ const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = merge(common, {
   mode: 'production',
   devtool: 'source-map',
+  plugins: [
+    new webpack.HashedModuleIdsPlugin(),
+  ],
   optimization: {
     minimizer: [
       new UglifyJsPlugin({
