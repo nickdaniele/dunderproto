5800197bce841ad37d115b9b256d52af817ae1e8
Nick Daniele <ngdaniele@gmail.com>
2018-11-17 15:22:06 -0800

    Created first react component.

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
-import React, { Component } from 'react';
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Main = () => <div>__proto__</div>;
+
+ReactDOM.render(<Main />, document.getElementById('main'));
