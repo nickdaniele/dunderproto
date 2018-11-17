dcdf30a469d58a7d7ff3c3c914a5d548226abbf6
Nick Daniele <ngdaniele@gmail.com>
2018-11-17 15:26:19 -0800

    Removed trailing commas.

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,15 +1,15 @@
+const webpack = require('webpack');
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
-const webpack = require('webpack');
 
 module.exports = merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
   plugins: [
-    new webpack.NamedModulesPlugin(),
+    new webpack.NamedModulesPlugin()
   ],
   devServer: {
     contentBase: './dist',
-    hot: true,
-  },
+    hot: true
+  }
 });
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,23 +1,23 @@
+const webpack = require('webpack');
 const merge = require('webpack-merge');
-const common = require('./webpack.common.js');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-const webpack = require('webpack');
+const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
   mode: 'production',
   devtool: 'source-map',
   plugins: [
-    new webpack.HashedModuleIdsPlugin(),
+    new webpack.HashedModuleIdsPlugin()
   ],
   optimization: {
     minimizer: [
       new UglifyJsPlugin({
         cache: true,
         parallel: true,
-        sourceMap: false,
+        sourceMap: false
       }),
-      new OptimizeCSSAssetsPlugin({}),
-    ],
-  },
+      new OptimizeCSSAssetsPlugin({})
+    ]
+  }
 });
